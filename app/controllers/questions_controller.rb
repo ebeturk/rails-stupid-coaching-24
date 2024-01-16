4d0dfd5a385f062e513f116e946e5d51df63dcc7
Emre Ebeturk <emreebeturk@gmail.com>
2024-01-16 23:09:47 +0100

    rubocop edit

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,17 +1,21 @@
 class QuestionsController < ApplicationController
   def ask; end
+
   def answer
     @user_question = params[:user_question]
-    @answer =
-      if @user_question == 'I am going to work'
-        'Great!'
-      elsif @user_question.end_with?('?')
-        'Silly question, get dressed and go to work!'
-      elsif @user_question == ""
-        @user_question = "You haven't asked any questions so..."
-        "I'll also keep quiet..."
-      else
-        'I don\'t care, get dressed and go to work!'
-      end
+    @answer = coach_answer
+  end
+
+  def coach_answer
+    if @user_question == 'I am going to work'
+      'Great!'
+    elsif @user_question.end_with?('?')
+      'Silly question, get dressed and go to work!'
+    elsif @user_question.blank?
+      @user_question = "You haven't asked any questions so..."
+      "I'll also keep quiet..."
+    else
+      'I don\'t care, get dressed and go to work!'
+    end
   end
 end
